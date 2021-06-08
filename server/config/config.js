e8ac346ac1fc902f23c1ea54a572a2bfc8d49a86
Daniel <dani.roj789@gmail.com>
2021-06-08 16:35:54 -0500

    JWT

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,18 @@
 //      PUERTO
 //====================
 process.env.PORT = process.env.PORT || 3000;
+
+//=======================
+// Vencimiento del Token
+//=======================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//=======================
+// SEED de autenticacion
+//=======================
+process.env.SEED = 'este-es-el-seed-desarrollo';
